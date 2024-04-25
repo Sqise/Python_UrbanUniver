c6f7f70fe36fa5dc8db4c886717b1c99579809cd
Sqise <167717729+Sqise@users.noreply.github.com>
2024-04-26 01:36:01 +0300

    Add files via upload

---
+++ homework3.py
@@ -0,0 +1,7 @@
+my_string = input('Введите строку: ')
+print('Длина строки (символов): ' , len(my_string))
+print('Верхний регистр: ' ,my_string .upper())
+print('Нижний регистр: ' ,my_string .lower())
+print('Без пробелов: ' ,my_string .replace(' ', ''))
+print('Первый символ строки: ', my_string[0])
+print('Последний символ строки: ', my_string[-1])
