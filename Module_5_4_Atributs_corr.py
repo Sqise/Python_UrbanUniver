15688b90fe1b9c3871e7b5d728222071b5fd2bb3
Sqise <167717729+Sqise@users.noreply.github.com>
2024-06-08 23:33:59 +0300

    Add files via upload

---
+++ Module_5_4_Atributs_corr.py
@@ -0,0 +1,17 @@
+# Создайте новый класс Buiding с атрибутом total
+# Создайте инициализатор для класса Buiding, который будет увеличивать
+# атрибут количества созданных объектов класса Building total
+# В цикле создайте 40 объектов класса Building и выведите их на экран командой print
+
+class Building:
+    total = 0
+
+    def __init__(self):
+        self.total += 1
+
+
+obj_list = []
+for i in range(40):
+    obj_list.append(i)
+    obj_list[i] = Building()
+print(obj_list)
