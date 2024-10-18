04124c9d182850a91765d019269effb04c1e2506
Sqise <167717729+Sqise@users.noreply.github.com>
2024-10-18 18:05:06 +0300

    Add files via upload

---
+++ 17_2/app/models/__init__.py
@@ -0,0 +1,5 @@
+#from user import User
+#from task import Task
+import sys
+sys.path.append('C://Users//STC//app')
+sys.path.append('F://_ Python//Urban//HomeWork//Module #17 - Бибилотеки ресурсы FastAPI. Часть 2//17_2')
