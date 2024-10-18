7ad889bc8c80c535349812430d40b005769191b9
Sqise <167717729+Sqise@users.noreply.github.com>
2024-10-18 17:29:16 +0300

    Add files via upload

---
+++ 17_2/app/models/__init__.py
@@ -0,0 +1,4 @@
+#from user import User
+#from task import Task
+import sys
+sys.path.append('F://_ Python//Urban//HomeWork//Module #17 - Бибилотеки ресурсы FastAPI. Часть 2//17_2')
---
+++ 17_2/app/models/user.py
@@ -0,0 +1,18 @@
+from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy.orm import relationship
+from sqlalchemy.schema import CreateTable
+from app.backend.db import Base
+
+
+class User(Base):
+    __tablename__ = 'users'
+    id = Column(Integer, index=True, primary_key=True, autoincrement=True)
+    username = Column(String, index=True)
+    firstname = Column(String)
+    lastname = Column(String)
+    age = Column(Integer)
+    user_id = Column(Integer, ForeignKey('users.id'))
+    slug = Column(String, unique=True, index=True)
+    tasks = relationship("Task", back_populates="user")
+
+print(CreateTable(User.__table__))
