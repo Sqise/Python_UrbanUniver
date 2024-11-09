645d40a6d0482f3e0a60292f7ce07ba03cedb65a
Sqise <167717729+Sqise@users.noreply.github.com>
2024-11-09 22:57:21 +0300

    Delete mod17 directory

--- mod17/app/models/__init__.py
+++
@@ -1,5 +0,0 @@
-#from user import User
-#from task import Task
-import sys
-sys.path.append('C://Users//STC//mod17')
-sys.path.append('F://_ Python//Urban//HomeWork//Module #17 - Бибилотеки ресурсы FastAPI. Часть 2//17_2')
